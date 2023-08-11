75ef9b14a442b037f3bda85674d83b5dfa7af21d
Arpan Savani <arpan22savani@outlook.com>
2023-08-11 16:24:32 -0700

    login

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 const db = require('../confg/database');
 
 /* localhost: 3000/user/register */
@@ -20,12 +21,14 @@ router.post('/register', async function(req, res, next) {
         console.log(`${email} already exists`);
         return res.redirect("/register");
       }
+      
+      var hashedPassword = await bcrypt.hash(password, 5);
 
       // insert into db
       var [insertResult, _] = await db.execute(
-        `INSERT INTO users (username, email, password) VALUE (?,?,?);`, [username, email, password]
+        `INSERT INTO users (username, email, password) VALUE (?,?,?);`, [username, email, hashedPassword]
       );
-      
+ 
       // respond
       if(insertResult && insertResult.affectedRows == 1) {
           return res.redirect('/login')
@@ -36,6 +39,34 @@ router.post('/register', async function(req, res, next) {
     } catch (err) { 
       next(err);
     } 
+});
+
+router.post('/login', async function (req,res, next) {
+  var { username, password } = req.body;
+  try {
+    var [results, _] = await db.execute(
+      `select id, username, email, password from users where username=?`,
+      [username]
+    );
+    const user = results[0];
+    if(!user){
+      return res.redirect("/login")
+    }
+
+    var passwordsMatch = await bcrypt.compare(password, user.password);
+    if (passwordsMatch) {
+      req.session.user = {
+          userId: user.id,
+          username: user.username,
+          email: user.email
+      }
+      return res.redirect("/");
+    } else {
+      return res.redirect("/login");
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 
