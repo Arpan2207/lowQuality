6f8f2cd2287f495dae25f3a04651abe014de8a5e
Arpan Savani <136035841+Arpan2207@users.noreply.github.com>
2023-12-19 11:13:32 -0800

    last comit frm M2

--- application/routes/index.js
+++ application/routes/index.js
@@ -5,24 +5,24 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.locals.title = "CSC 317 app"
   res.locals.name = "Arpan R Savani" 
-  res.render("index",{title:"Home", css: ["index.css"], js: ["index.js"]});
+  res.render("index",{title:"Home", css: ["index.css", "navbar.css"], js: ["index.js"]});
 });
 
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {title:"Login", css: ["login.css"]}) 
+  res.render('login', {title:"Login", css: ["login.css" , "navbar.css"]}) 
 }); 
 
 router.get('/postvideo', function(req, res, next) {
-  res.render('postvideo', {title:"Postvideo", css: ["postvideo.css"]}) 
+  res.render('postvideo', {title:"Postvideo", css: ["postvideo.css", "navbar.css"]}) 
 }); 
 
 router.get('/register', function(req, res, next) {
-  res.render('register', {title:"Register", css: ["register.css"], /*js: ["register.js"]*/}) 
+  res.render('register', {title:"Register", css: ["register.css", "navbar.css"], /*js: ["register.js"]*/}) 
 }); 
 
 router.get('/viewpost', function(req, res, next) {
-  res.render('viewpost', {title:"Viewpost", css: ["viewpost.css"]}) 
+  res.render('viewpost', {title:"Viewpost", css: ["viewpost.css", "navbar.css"]}) 
 }); 
 
 module.exports = router;
